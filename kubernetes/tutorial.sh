2fdc9154c36b3f4fded7070ae9506c5bae108b29
Juliano Atanazio <juliano777@gmail.com>
2019-05-27 16:42:48 -0300

    Directories organization

---
+++ kubernetes/tutorial.sh
@@ -0,0 +1,24 @@
+sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+
+
+# on kubernetes master and kubernetes node
+
+sudo yum install -y ebtables ethtool kubelet
+
+
+# on Kubernetes master
+
+sudo yum install -y kubeadm kubectl
+
+
+
+
