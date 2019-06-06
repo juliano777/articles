88fbc4ba9a8bc6696fa0e3f0e7948c62ab6973ea
Juliano Atanazio <juliano777@gmail.com>
2019-06-06 14:29:46 -0300

    20190606_1414

--- R/building_r.sh
+++ R/building_r.sh
@@ -1,1 +1,21 @@
-dssd
+# Install packages to build Python:
+
+aptitude install -y libffi-dev
+
+yum install -y libffi-devel
+
+
+
+# Python Version environment:
+
+read -p 'What is the Python version (X.Y.Z) to download? ' PYTHON_VERSION
+
+
+
+# Download the Python source code:
+
+cd /tmp
+
+wget -c https://www.python.org/ftp/python/${PYTHON_VERSION}/\
+Python-${PYTHON_VERSION}.tar.xz
+
