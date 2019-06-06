03d5cb17c1b6568f2a6bfa141d26d48d40410d71
Juliano Atanazio <juliano777@gmail.com>
2019-06-06 10:30:03 -0300

    Python

---
+++ python/building_python.sh
@@ -0,0 +1,82 @@
+# Install packages to build Python:
+
+aptitude install -y libffi-dev
+
+
+
+# Python Version environment:
+
+read -p 'What is the Python version (X.Y.Z) to download? ' PYTHON_VERSION
+
+
+
+# Download the Python source code:
+
+cd /tmp
+
+wget -c https://www.python.org/ftp/python/${PYTHON_VERSION}/\
+Python-${PYTHON_VERSION}.tar.xz
+
+
+
+# Unpack the file and enter into directory:
+
+tar xf Python-${PYTHON_VERSION}.tar.xz && cd Python-${PYTHON_VERSION}/
+
+
+
+# Número de jobs conforme a quantidade cores de CPU (cores + 1):
+
+NJOBS=`expr \`cat /proc/cpuinfo | egrep ^processor | wc -l\` + 1`
+
+
+
+# Make Options:
+
+MAKEOPTS="-j${NJOBS}"
+
+
+
+# Configure:
+
+./configure \
+    --prefix /usr/local/python \
+    --includedir /usr/local/include
+
+
+
+# Make
+
+make && make altinstall
+
+
+
+# Profile file:
+
+cat << EOF > /etc/profile.d/python.sh
+#!/bin/bash
+
+export PATH="/usr/local/python/bin:\${PATH}"
+EOF
+
+
+
+# Make a tar package:
+
+tar cvf /tmp/Python-bin-${PYTHON_VERSION}.tar \
+/etc/profile.d/python.sh \
+/usr/local/{python,include}
+
+
+
+# Compact with XZ:
+
+xz -9 /tmp/Python-bin-${PYTHON_VERSION}.tar
+
+
+
+
+
+
+
+
