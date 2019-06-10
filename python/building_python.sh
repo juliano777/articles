b0ad73734981eef94db36f1c9e89de9e9d65c229
Juliano Atanazio <juliano777@gmail.com>
2019-06-10 09:17:10 -0300

    20190610_0909

--- python/building_python.sh
+++ python/building_python.sh
@@ -1,7 +1,7 @@
 # Install packages to build Python:
 
 apt update && apt install -y \
-    lib{ffi6,curses5} \
+    lib{ffi6,ncurses5} \
     {libbz2,zlib1g,libssl,libncurses,libffi,uuid,tk}-dev \
     wget gcc g++ make xz-utils
 
