fbe6e7604b5d6a00e4720e7885d0c21e1030cfd9
Juliano Atanazio <juliano777@gmail.com>
2019-06-24 09:15:55 -0300

    20190624_0909

--- postgresql/audit.sql
+++ postgresql/audit.sql
@@ -8,4 +8,15 @@ CREATE TABLE tb_user(
     password VARCHAR(12) NOT NULL,
     active boolean DEFAULT TRUE);
 
-CREATE TABLE tb_user_audit (
+CREATE TABLE tb_user_audit(
+    id int,
+    name VARCHAR(50) NOT NULL,
+    password VARCHAR(12) NOT NULL,
+    active boolean,
+    modif_ts TIMESTAMP WITH TIME ZONE,
+    operation CHAR(1));
+
+I
+U
+D
+
