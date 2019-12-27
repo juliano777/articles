37a6c42e228a89d4f08fd83d96e624fab4534e11
Juliano Atanazio <juliano777@gmail.com>
2019-12-27 15:01:54 -0300

    Install

---
+++ ansible/00_install.sh
@@ -0,0 +1,8 @@
+# (CentOS) Install Python 3:
+sudo dnf install -y python3{,-setuptools} wget && sudo dnf clean all
+
+# Install pip:
+wget -O - https://bootstrap.pypa.io/get-pip.py | sudo python3
+
+# Install Ansible
+sudo pip3 install ansible
