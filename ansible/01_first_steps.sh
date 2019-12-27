cab4c290209571ad40555116770a4e284b4a68b9
Juliano Atanazio <juliano777@gmail.com>
2019-12-27 17:17:52 -0300

    Playbooks

--- ansible/01_first_steps.sh
+++ ansible/01_first_steps.sh
@@ -44,3 +44,49 @@ vb0 | SUCCESS => {
     "ping": "pong"
 }
 '
+
+#
+ansible vb0 -b -u root -a 'whoami'
+
+'
+vb0 | CHANGED | rc=0 >>
+root
+'
+
+# 
+touch /tmp/foo.txt
+
+#
+ansible vb0 -m copy -a 'src=/tmp/foo.txt dest=/tmp/bar.xyz mode=600'
+
+'
+vb0 | CHANGED => {
+    "ansible_facts": {
+        "discovered_interpreter_python": "/usr/libexec/platform-python"
+    },
+    "changed": true,
+    "checksum": "da39a3ee5e6b4b0d3255bfef95601890afd80709",
+    "dest": "/tmp/bar.xyz",
+    "gid": 1000,
+    "group": "centos",
+    "md5sum": "d41d8cd98f00b204e9800998ecf8427e",
+    "mode": "0600",
+    "owner": "centos",
+    "secontext": "unconfined_u:object_r:user_home_t:s0",
+    "size": 0,
+    "src": "/home/centos/.ansible/tmp/ansible-tmp-1577477311.5657377-151019920460756/source",
+    "state": "file",
+    "uid": 1000
+}
+'
+
+#
+ansible vb0 -a 'ls -lh /tmp/bar.xyz'
+
+'
+vb0 | CHANGED | rc=0 >>
+-rw-------. 1 centos centos 0 Dec 27 18:08 /tmp/bar.xyz
+'
+
+#
+ansible-playbook first_playbook.yaml
