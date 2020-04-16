e6393db873efcbc7d5f2f401e5d5731b85b73cc5
Juliano Atanazio <juliano777@gmail.com>
2020-04-16 08:55:56 -0300

    .

--- mongodb/test.sh
+++ mongodb/test.sh
@@ -35,27 +35,32 @@ EOF
 
 
 # Update repository list
-dnf repolist
+sudo dnf repolist
 
 
 
 # Install MongoDB
-dnf install -y mongodb-org && dnf clean all
+sudo dnf install -y mongodb-org && sudo dnf clean all
 
 
 
 # Edit the configuration file
-vim /etc/mongod.conf
+sudo vim /etc/mongod.conf
 
 "
-coment the line 'bindIp: ...'
-add just below, in the same indentation 'bindIpAll: true'
+coment or delete the line 'bindIp: ...'
+add just below, in the same indentation 'bindIpAll: true' like this:
+
+# network interfaces
+net:
+  port: 27017
+  bindIpAll: true
 "
 
 
 
 # Enable and start immediately the MongoDB service
-systemctl enable --now mongod
+sudo systemctl enable --now mongod
 
 
 
@@ -67,7 +72,7 @@ systemctl enable --now mongod
 
 
 # Edit the configuration file
-vim /etc/mongod.conf
+sudo vim /etc/mongod.conf
 
 
 
@@ -105,7 +110,7 @@ db.auth('admin', passwordPrompt())
 
 
 # Restart MongoDB service
-systemctl restart mongod
+sudo systemctl restart mongod
 
 
 
@@ -116,11 +121,47 @@ systemctl restart mongod
 # Ref.: https://docs.mongodb.com/manual/tutorial/deploy-replica-set/
 
 
+
+# Directory creation for security purposes (permission mode 700)
 sudo mkdir -m 700 /var/lib/mongo/security
+
+
+
+# Generate the keyfile
 sudo su -c 'openssl rand -base64 756 > /var/lib/mongo/security/keyfile'
+
+
+
+# Change its permission mode to 400
 sudo chmod 400 /var/lib/mongo/security/keyfile
+
+
+
+# Change ownership
 sudo chown -R mongod: /var/lib/mongo/security
-sudo rsync -av /var/lib/mongo/security root@mongo-03:/var/lib/mongo/
+
+
+
+# Shell script for get the address of each node (except master node)
+i=0
+
+while :; do 
+  read -p '[<Ctrl> + C to finish] - Enter hostname or IP adrress: ' NODE[i];
+  let i+=1;
+done
+
+
+
+# Script for send the keyfile to worker nodes
+for i in `seq 0 $((${#NODE[@]}-1))`; do
+  echo -e "\n${NODE[$i]}\n"
+  sudo rsync -av /var/lib/mongo/security root@${NODE[i]}:/var/lib/mongo/;
+done
+
+
+
+
+
 sudo systemctl stop mongod.service
 
 # Edit the configuration file
@@ -137,9 +178,10 @@ security:
   keyFile: /var/lib/mongo/security/keyfile
 "
 
+
+
+# Start the MongoDB service
 systemctl start mongod
-
-
 
 
 
